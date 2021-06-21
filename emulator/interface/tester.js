993d8d1abb34fbc93c028af81ad81fbadc003d9c
RohitEdathil <44530239+RohitEdathil@users.noreply.github.com>
2021-06-21 23:11:56 +0530

    device update and register complete

--- emulator/interface/tester.js
+++ emulator/interface/tester.js
@@ -4,10 +4,30 @@ const PORT = "8080";
 function $(id) {
   return document.getElementById(id);
 }
-
+function sync() {
+  if (ws.readyState == 1) {
+    ws.send(format(["update", { o2: $("o2").value, ph: $("ph").value }]));
+  }
+}
+function update(of) {
+  $(of + "-label").innerHTML = `${of} : ${$(of).value}`;
+}
+function format(data) {
+  return JSON.stringify(data);
+}
 let ws = new WebSocket(`ws://${HOST}:${PORT}`);
 
-ws.onopen = (m) => console.log("Connected");
+ws.onopen = (m) => {
+  $("state").innerHTML = "Connected ✅";
+  $("sync").disabled = false;
+  ws.send(format(["register", "device"]));
+};
 ws.onmessage = (m) => console.log(m.data);
-ws.onclose = (m) => console.log("Closed");
-ws.onerror = (e) => console.log(e);
+ws.onclose = (m) => {
+  $("state").innerHTML = "Disconnected ❌";
+  $("sync").disabled = true;
+};
+ws.onerror = (e) => {
+  $("state").innerHTML = "Error ⚠";
+  console.log(e);
+};
--- emulator/server/emulator.py
+++ emulator/server/emulator.py
@@ -1,26 +1,53 @@
 import asyncio
+import sys
 import websockets
 import socket
+from os import system
 from json import loads, dumps
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 8080
+system('cls')
 
 client = None
 device = None
 
 
+def register(message: dict, websocket: websockets.WebSocketServerProtocol):
+    if message[1] == 'device':
+        global device
+        device = websocket
+        print(websocket.remote_address, "Registered as Device")
+    else:
+        global client
+        client = websocket
+        print(websocket.remote_address, "Registered as Client")
+
+
+def update(message: dict, websocket: websockets.WebSocketServerProtocol):
+    global client
+    if client != None:
+        print('Device +-(update)--> Client :', message[1])
+    else:
+        print('Device +-(update)--> [Client Inactive] :', message[1])
+
+
+command_map = {
+    'register': register,
+    'update': update,
+}
+
+
 def handle(message: str, websocket: websockets.WebSocketServerProtocol):
-    data = loads(message)
-    print(data)
+    message = loads(message)
+    command_map.get(message[0], lambda a, b: print(
+        f'{b.remote_address} : Invalid Message'))(message, websocket)
 
 
 async def run(websocket: websockets.WebSocketServerProtocol, path: str):
     if websocket.state == 1:
-        print(websocket.remote_address, " : Connected")
+        print(websocket.remote_address, ": Connected")
     async for message in websocket:
-
-        await websocket.send(message)
-
+        handle(message, websocket)
 
 print(f"Running @: ws://{HOST}:{PORT}")
 asyncio.get_event_loop().run_until_complete(
