85d2ab3227cb95d91b120a1220a73c672820f45a
RohitEdathil <44530239+RohitEdathil@users.noreply.github.com>
2021-06-22 14:06:16 +0530

    time and sensor data comms done

--- emulator/interface/tester.js
+++ emulator/interface/tester.js
@@ -1,6 +1,9 @@
-const HOST = "192.168.43.29";
+const HOST = "192.168.17.32";
 const PORT = "8080";
-
+var current_hours = 4;
+var current_minutes = 0;
+var error = "no-error";
+let ws;
 function $(id) {
   return document.getElementById(id);
 }
@@ -9,25 +12,86 @@ function sync() {
     ws.send(format(["update", { o2: $("o2").value, ph: $("ph").value }]));
   }
 }
+function send_time() {
+  ws.send(format(["time", { hours: current_hours, minutes: current_minutes }]));
+}
+function send_error() {
+  ws.send(format(["error", error]));
+}
+function setTime(hours, minutes) {
+  current_hours = parseInt(hours);
+  current_minutes = parseInt(minutes);
+  $("from").innerHTML = `[H: ${current_hours} | M: ${current_minutes}]`;
+  $("to").innerHTML = `[H: ${current_hours + 18} | M: ${current_minutes}]`;
+  $("in").style.left = `${
+    ((current_hours * 60 + current_minutes) * 150) / 360
+  }px`;
+}
+function set(data) {
+  switch (data[1]) {
+    case "time":
+      setTime(data[2]["hours"], data[2]["minutes"]);
+      break;
+
+    default:
+      break;
+  }
+}
 function update(of) {
   $(of + "-label").innerHTML = `${of} : ${$(of).value}`;
 }
+
 function format(data) {
   return JSON.stringify(data);
 }
-let ws = new WebSocket(`ws://${HOST}:${PORT}`);
-
-ws.onopen = (m) => {
-  $("state").innerHTML = "Connected ✅";
-  $("sync").disabled = false;
-  ws.send(format(["register", "device"]));
-};
-ws.onmessage = (m) => console.log(m.data);
-ws.onclose = (m) => {
-  $("state").innerHTML = "Disconnected ❌";
-  $("sync").disabled = true;
-};
-ws.onerror = (e) => {
-  $("state").innerHTML = "Error ⚠";
-  console.log(e);
-};
+function request(data) {
+  switch (data[1]) {
+    case "all":
+      sync();
+      send_error();
+      send_time();
+      break;
+    case "update":
+      sync();
+      break;
+    case "time":
+      send_time();
+      break;
+    case "error":
+      send_error();
+      break;
+    default:
+      break;
+  }
+}
+function handle(message) {
+  const data = JSON.parse(message.data);
+  const command_map = {
+    request: request,
+    set: set,
+  };
+  command_map[data[0]](data);
+}
+function connect() {
+  ws = new WebSocket(`ws://${HOST}:${PORT}`);
+  ws.onopen = (m) => {
+    $("state").innerHTML = "Connected ✅";
+    $("sync").disabled = false;
+    ws.send(format(["register", "device"]));
+  };
+  ws.onmessage = (m) => handle(m);
+  ws.onclose = (m) => {
+    $("state").innerHTML = "Disconnected ❌";
+    $("sync").disabled = true;
+    setTimeout(() => {
+      $("state").innerHTML = "Connecting ⌚";
+      connect();
+    }, 3000);
+  };
+  ws.onerror = (e) => {
+    $("state").innerHTML = "Error ⚠";
+    console.log(e);
+  };
+}
+connect();
+setTime(current_hours, current_minutes);
--- emulator/server/emulator.py
+++ emulator/server/emulator.py
@@ -12,7 +12,7 @@ client = None
 device = None
 
 
-def register(message: dict, websocket: websockets.WebSocketServerProtocol):
+async def register(message: dict, websocket: websockets.WebSocketServerProtocol):
     if message[1] == 'device':
         global device
         device = websocket
@@ -23,31 +23,71 @@ def register(message: dict, websocket: websockets.WebSocketServerProtocol):
         print(websocket.remote_address, "Registered as Client")
 
 
-def update(message: dict, websocket: websockets.WebSocketServerProtocol):
+async def error(message: dict, websocket: websockets.WebSocketServerProtocol):
     global client
     if client != None:
-        print('Device +-(update)--> Client :', message[1])
+        print('Device ---error----> Client :', message[1])
+        await client.send(dumps(message))
     else:
-        print('Device +-(update)--> [Client Inactive] :', message[1])
+        print('Device ---error--> [Client Inactive] :', message[1])
+
+
+async def update(message: dict, websocket: websockets.WebSocketServerProtocol):
+    global client
+    if client != None:
+        print('Device ---update--> Client :', message[1])
+        await client.send(dumps(message))
+    else:
+        print('Device ---update--> [Client Inactive] :', message[1])
+
+
+async def get_time(message: dict, websocket: websockets.WebSocketServerProtocol):
+    global client
+    if client != None:
+        print('Device ---time----> Client :', message[1])
+        await client.send(dumps(message))
+    else:
+        print('Device ---time--> [Client Inactive] :', message[1])
+
+
+async def request(message: dict, websocket: websockets.WebSocketServerProtocol):
+    global device
+    if device != None:
+        print('Device <--request-- Client :', message[1])
+        await device.send(dumps(message))
+    else:
+        print('[Device Inactive] <--request-- Client :', message[1])
+
+
+async def set_value(message: dict, websocket: websockets.WebSocketServerProtocol):
+    global device
+    if device != None:
+        print('Device <--set------ Client :', message[1], message[2])
+        await device.send(dumps(message))
+    else:
+        print('[Device Inactive] <--set---- Client :', message[1], message[2])
 
 
 command_map = {
     'register': register,
     'update': update,
+    'request': request,
+    'set': set_value,
+    'time': get_time,
+    'error': error
 }
 
 
-def handle(message: str, websocket: websockets.WebSocketServerProtocol):
+async def handle(message: str, websocket: websockets.WebSocketServerProtocol):
     message = loads(message)
-    command_map.get(message[0], lambda a, b: print(
-        f'{b.remote_address} : Invalid Message'))(message, websocket)
+    await command_map.get(message[0])(message, websocket)
 
 
 async def run(websocket: websockets.WebSocketServerProtocol, path: str):
     if websocket.state == 1:
         print(websocket.remote_address, ": Connected")
     async for message in websocket:
-        handle(message, websocket)
+        await handle(message, websocket)
 
 print(f"Running @: ws://{HOST}:{PORT}")
 asyncio.get_event_loop().run_until_complete(
