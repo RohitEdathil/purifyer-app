8b1dc50430de576d5d54701a58ff765e3d671e0d
RohitEdathil <44530239+RohitEdathil@users.noreply.github.com>
2021-06-26 14:06:08 +0530

    logger fix

--- emulator/server/emulator.py
+++ emulator/server/emulator.py
@@ -23,60 +23,60 @@ async def register(message: dict, websocket: websockets.WebSocketServerProtocol)
     if message[1] == 'device':
         global device
         device = websocket
-        log(websocket.remote_address, "Registered as Device")
+        await log(websocket.remote_address, "Registered as Device")
     elif message[1] == 'logger':
         global logger
         logger = websocket
-        log(websocket.remote_address, "Registered as Logger")
+        await log(websocket.remote_address, "Registered as Logger")
     else:
         global client
         client = websocket
-        log(websocket.remote_address, "Registered as Client")
+        await log(websocket.remote_address, "Registered as Client")
 
 
 async def error(message: dict, websocket: websockets.WebSocketServerProtocol):
     global client
     if client != None:
-        log('Device ---error----> Client :', message[1])
+        await log('Device ---error----> Client :', message[1])
         await client.send(dumps(message))
     else:
-        log('Device ---error--> [Client Inactive] :', message[1])
+        await log('Device ---error--> [Client Inactive] :', message[1])
 
 
 async def update(message: dict, websocket: websockets.WebSocketServerProtocol):
     global client
     if client != None:
-        log('Device ---update--> Client :', message[1])
+        await log('Device ---update--> Client :', message[1])
         await client.send(dumps(message))
     else:
-        log('Device ---update--> [Client Inactive] :', message[1])
+        await log('Device ---update--> [Client Inactive] :', message[1])
 
 
 async def get_time(message: dict, websocket: websockets.WebSocketServerProtocol):
     global client
     if client != None:
-        log('Device ---time----> Client :', message[1])
+        await log('Device ---time----> Client :', message[1])
         await client.send(dumps(message))
     else:
-        log('Device ---time--> [Client Inactive] :', message[1])
+        await log('Device ---time--> [Client Inactive] :', message[1])
 
 
 async def request(message: dict, websocket: websockets.WebSocketServerProtocol):
     global device
     if device != None:
-        log('Device <--request-- Client :', message[1])
+        await log('Device <--request-- Client :', message[1])
         await device.send(dumps(message))
     else:
-        log('[Device Inactive] <--request-- Client :', message[1])
+        await log('[Device Inactive] <--request-- Client :', message[1])
 
 
 async def set_value(message: dict, websocket: websockets.WebSocketServerProtocol):
     global device
     if device != None:
-        log('Device <--set------ Client :', message[1], message[2])
+        await log('Device <--set------ Client :', message[1], message[2])
         await device.send(dumps(message))
     else:
-        log('[Device Inactive] <--set---- Client :', message[1], message[2])
+        await log('[Device Inactive] <--set---- Client :', message[1], message[2])
 
 
 command_map = {
@@ -96,7 +96,7 @@ async def handle(message: str, websocket: websockets.WebSocketServerProtocol):
 
 async def run(websocket: websockets.WebSocketServerProtocol, path: str):
     if websocket.state == 1:
-        log(websocket.remote_address, ": Connected")
+        await log(websocket.remote_address, ": Connected")
     async for message in websocket:
         await handle(message, websocket)
 
