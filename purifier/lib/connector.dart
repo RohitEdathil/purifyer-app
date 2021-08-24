4c5e37fd63ab290618014c12a6d9a29388c3a118
Rohit V <rohitedathil@gmail.com>
2021-08-24 22:49:20 +0530

    bluetooth introduced

--- purifier/lib/connector.dart
+++ purifier/lib/connector.dart
@@ -1,20 +1,99 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:purifier/uuids.dart';
 
 class Connector extends ChangeNotifier {
   bool connected = false;
+
   double ph = 7;
   int o2 = 23;
-  int time = 120;
   ErrorType error = ErrorType.noError;
+  int time = 120;
+  BluetoothCharacteristic? scheduleCharacteristic;
 
-  Future<bool> connect() async {
+  Future<void> connect() async {
     final bluetooth = FlutterBlue.instance;
-    bluetooth.startScan(timeout: Duration(seconds: 10));
-    bluetooth.scanResults.listen((results) {
-      print(results);
+    print(await bluetooth.connectedDevices);
+    await bluetooth.startScan(
+      timeout: Duration(seconds: 10),
+      withServices: [TIME, SENSORS, ERROR],
+    );
+    bluetooth.scanResults.listen((results) async {
+      if (results.isNotEmpty) {
+        final device = results[0].device;
+        await device.connect(timeout: Duration(seconds: 10));
+
+        device.state.listen((state) async {
+          if (state == BluetoothDeviceState.disconnected) {
+            disconnected();
+          }
+        });
+
+        device.discoverServices().then((services) {
+          services.forEach((service) {
+            // Time Service
+            if (service.uuid == TIME) {
+              service.characteristics.forEach((characteristic) async {
+                // Clock Syncing
+                if (characteristic.uuid == CLOCK) {
+                  final time = DateTime.now().toUtc();
+                  await characteristic.write([
+                    time.year,
+                    time.month,
+                    time.day,
+                    time.hour,
+                    time.minute,
+                    time.second,
+                  ]);
+                }
+                // Schedule Characteristic
+                if (characteristic.uuid == SCHEDULE) {
+                  scheduleCharacteristic = characteristic;
+                  final current = await characteristic.read();
+                  time = current[0] * 60 + current[1];
+                }
+              });
+            }
+
+            // Sensors Service
+            if (service.uuid == SENSORS) {
+              service.characteristics.forEach((characteristic) {
+                // Ph Characteristic
+                if (characteristic.uuid == PH) {
+                  characteristic.value.listen((value) async {
+                    ph = value[0] / 10;
+                    notifyListeners();
+                  });
+                }
+                // O2 Characteristic
+                if (characteristic.uuid == O2) {
+                  characteristic.value.listen((value) async {
+                    o2 = value[0];
+                    notifyListeners();
+                  });
+                }
+              });
+            }
+
+            // Error Service
+            if (service.uuid == ERROR) {
+              service.characteristics.forEach((characteristic) {
+                // Error Characteristic
+                if (characteristic.uuid == CODE) {
+                  characteristic.value.listen((value) async {
+                    error = errorType(value[0]);
+                  });
+                }
+              });
+            }
+          });
+        });
+        connected = true;
+        bluetooth.stopScan();
+        notifyListeners();
+      }
     });
-    return false;
+    bluetooth.stopScan();
   }
 
   void setTime(int t) {
@@ -22,9 +101,25 @@ class Connector extends ChangeNotifier {
     notifyListeners();
   }
 
+  void disconnected() {
+    connected = false;
+    notifyListeners();
+  }
+
   void sendTime() {
-    print("*Sends time*");
+    scheduleCharacteristic?.write([time ~/ 60, time % 60]);
   }
 }
 
-enum ErrorType { noError, phError, otherError }
+enum ErrorType { noError, phError, unknownError }
+
+ErrorType errorType(int code) {
+  switch (code) {
+    case 0:
+      return ErrorType.noError;
+    case 1:
+      return ErrorType.phError;
+    default:
+      return ErrorType.unknownError;
+  }
+}
--- purifier/lib/views/loading.dart
+++ purifier/lib/views/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:purifier/connector.dart';
@@ -6,21 +7,53 @@ import 'package:purifier/views/home.dart';
 
 class LoadingView extends StatelessWidget {
   void connect(BuildContext context) {
-    Provider.of<Connector>(context, listen: false)
-        .connect()
-        .then((value) => print(value));
+    Provider.of<Connector>(context, listen: false).connect();
   }
 
   @override
   Widget build(BuildContext context) {
+    connect(context);
+    if (Provider.of<Connector>(context).connected) return HomeView();
+
+    final blu = FlutterBlue.instance;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Stack(
-          children: [
-            Center(child: Lottie.asset('assets/connecting.json')),
-          ],
-        ),
+      body: StreamBuilder<BluetoothState>(
+        stream: blu.state,
+        builder: (context, snapshot) => snapshot.data == BluetoothState.on
+            ? Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                    child: StreamBuilder<bool>(
+                  stream: blu.isScanning,
+                  builder: (context, snapshot) => snapshot.data ?? true
+                      ? Lottie.asset('assets/connecting.json')
+                      : Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Scan failed.',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.headline3,
+                            ),
+                            IconButton(
+                              onPressed: () => connect(context),
+                              icon: Icon(Icons.refresh_rounded),
+                              color: Theme.of(context).primaryColor,
+                              iconSize: 40,
+                            )
+                          ],
+                        ),
+                )),
+              )
+            : Center(
+                child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'Please turn on Bluetooth.',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              )),
       ),
     );
   }
